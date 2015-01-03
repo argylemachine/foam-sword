f401762d0c60fb7510678de58e60335522735b49
Robert Keizer <robert@keizer.ca>
2015-01-03 12:12:22 -0600

    Some logic about making a request.

--- frontend/app/controllers/data/add.js
+++ frontend/app/controllers/data/add.js
@@ -7,8 +7,21 @@ export default Ember.Controller.extend( {
 		submitUrl: function( ){
 			var _url	= this.get( "url" );
 			var _type	= this.get( "type" );
+			var _data	= this.get( "data" );
 
-			console.log( "I have type of " + _type );
+			var _method;
+			if( _type === "GET" ){
+				_method = $.get;
+			}else if( _type === "POST" ){
+				_method = $.post;
+			}else{
+				console.log( "What the hell is _type: " + _type );
+			}
+
+			var _request = _method( _url, _data, function( rawResponse ){
+				self.set( "rawResponse", rawResponse );
+				self.set( "showStepTwo", true );
+			} );
 		}
 	}
 } );
