860280ba55813984a0adb12f407a845ebd1671e5
Robert Keizer <robert@keizer.ca>
2015-01-03 22:08:34 -0600

    Working away. Going to have to split out again from mixin into seperate basically identical components.

--- frontend/app/components/data-select-children.js
+++ frontend/app/components/data-select-children.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
+import dataSelectComponentMixin from '../mixins/data-select-component-mixin';
 
-export default Ember.Component.extend( {
+export default Ember.Component.extend( dataSelectComponentMixin, {
+
+	dataPart: [ ],
+
 	didInsertElement: function( ){
 
 		// Hide the dom elements that are marked as hidden right away.
 		$( "#" + this.get( "elementId" ) ).find( "hidden" ).each( function( ){
 			$(this).hide();
 		} );
-	}
+
+		this.splitData( );
+	},
+
+	hookOnData: function( ){
+		console.log( "This is data-select-children and I have new data: ");
+		console.log( this.get("dataPart") );
+	}.observes( "dataPart" )
 } );
