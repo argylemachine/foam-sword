f01a5404be485e09cb8d5d52b4def41474de7d56
Robert Keizer <robert@keizer.ca>
2015-01-04 11:03:51 -0600

    Working hook on checkboxes; function being called when checked or unchecked.

--- frontend/app/components/data-select-children.js
+++ frontend/app/components/data-select-children.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import dataSelectComponentMixin from '../mixins/data-select-component-mixin';
 
-export default Ember.Component.extend( dataSelectComponentMixin, {
+export default Ember.Component.extend( {
 
 	dataPart: [ ],
 
@@ -11,12 +10,5 @@ export default Ember.Component.extend( dataSelectComponentMixin, {
 		$( "#" + this.get( "elementId" ) ).find( "hidden" ).each( function( ){
 			$(this).hide();
 		} );
-
-		this.splitData( );
-	},
-
-	hookOnData: function( ){
-		console.log( "This is data-select-children and I have new data: ");
-		console.log( this.get("dataPart") );
-	}.observes( "dataPart" )
+	}
 } );
--- frontend/app/components/data-select.js
+++ frontend/app/components/data-select.js
@@ -1,18 +1,98 @@
 import Ember from 'ember';
 import RandomIdMixin from '../mixins/random-id';
-import dataSelectComponentMixin from '../mixins/data-select-component-mixin';
 
-export default Ember.Component.extend( RandomIdMixin, dataSelectComponentMixin, {
+export default Ember.Component.extend( RandomIdMixin, {
+
+	dataPart: [ ],
+
+	didInsertElement: function( ){
+		var self	= this;
+
+		// Go through and parse the data into dataPart so that it can be used
+		// in handlebars.
+		var _dataPart	= [ ];
+		Object.keys( this.get( "data" ) ).forEach( function( key ){
+			_dataPart.push( { name: key, checked: false, children: self.getChildren( self.get("data")[key] ), randomId: self.get("randomId") } );
+		} );
+		this.set( "dataPart", _dataPart );
+
+		// We want to make sure we hook on checkbox select.
+		self.hookCheckboxes( );
+	},
+
+	getChildren: function( data ){
+		var self = this;
+		try{
+			if( Array.isArray( data ) ){
+				return [ ];
+			}else if( typeof data !== "object" ){
+				return [ ];
+			}
+
+			var _return = [ ];
+			Object.keys( data ).forEach( function( key ){
+				_return.push( { name: key, checked: false, children: self.getChildren( data[key] ), randomId: self.get("randomId") } );
+			} );
+			return _return;
+		}catch( err ){
+			return [ ];
+		}
+	},
+
+	hookCheckboxes: function( rootElement ){
+
+		var self = this;
+
+		if( !rootElement ){
+			rootElement = this.get( "elementId" );
+		}
+
+		// Checkboxes that we can see currently.
+		$("#" + rootElement ).find( ":checkbox" ).each( function( ){
+
+			// If we already have a hook on this checkbox
+			// lets ignore it.
+			if( $(this).data( ).hooked ){ return; }
+
+			// Mark this checkbox as being hooked.
+			$(this).data( "hooked", true );
+
+			// Setup the change listener
+			$(this).change( function( ){
+				if( $(this).is( ":checked" ) ){
+					console.log( "I have checked of " );
+					console.log( this );
+				}else{
+					// Uncheck already selected item.
+				}
+			} );
+		} );
+
+		// Lets setup a DOMSubtreeModified event listener so that we 
+		// make sure to hook on any new checkboxes that get shoved in.
+		$("#" + rootElement ).one( "DOMSubtreeModified", function( ){
+			self.hookCheckboxes( rootElement );
+		} );
+	},
+
 	actions: {
 		expandClicked: function( id ){
 			var _dom = $("#children-"+id);
 			if( _dom.hasClass( "hidden" ) ){
 				$("#link-"+id).html("Hide");
 				_dom.removeClass('hidden');
+
+				// We want to enable checkboxes below this.
+				this.hookCheckboxes( "children-"+id );
+
 			}else{
 				$("#link-"+id).html("Expand");
 				_dom.addClass("hidden");
 			}
+		},
+		selectData: function( details ){
+			console.log( "This is selectData; I have details of " );
+			console.log( details );
 		}
 	}
 } );
